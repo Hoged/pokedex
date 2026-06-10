78a793873d2f10e50aef6808708ecb5dfe5f9089
Hoged <samu.junttila@gmail.com>
2026-06-10 11:32:07 +0300

    cleanup of initial setup

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-	"strings"
-)
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		rawInput := scanner.Text()
-		commands := strings.Fields(strings.ToLower(rawInput))
-		fmt.Printf("Your command was: %v\n", commands[0])
-	}
+	startRepl()
 }
 
--- repl.go
+++ repl.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"bufio"
+	"os"
 	"strings"
 )
 
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		userInput := cleanInput(scanner.Text())
+		if len(userInput) == 0 {
+			continue
+		}
+
+		command := userInput[0]
+
+		fmt.Printf("Your command was: %v\n", command)
+	}
+}
+
 func cleanInput(text string) []string {
 	cleanText := strings.Fields(strings.ToLower(text))
-
 	return cleanText
 }
